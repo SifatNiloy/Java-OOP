c7830c46e1ba58ee897564a5ee98337e9406e40e
SifatNiloy <SifatNiloy18@gmail.com>
2024-02-13 19:19:19 +0600

    pushing my java codes

---
+++ Java_Beginner/src/Java_Basic/Java_practise.java
@@ -0,0 +1,10 @@
+package Java_Basic;
+
+public class Java_practise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
