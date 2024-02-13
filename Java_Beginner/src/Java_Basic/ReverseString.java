f74716874b1fa231b1fdd90c8858bb7b625cde44
SifatNiloy <SifatNiloy18@gmail.com>
2024-02-13 20:13:49 +0600

    reversed string

---
+++ Java_Beginner/src/Java_Basic/ReverseString.java
@@ -0,0 +1,16 @@
+package Java_Basic;
+import java.util.Scanner;
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String input = "Independent";
+	    StringBuilder stringBuildervarible = new StringBuilder();
+	    stringBuildervarible.append(input);
+	    stringBuildervarible.reverse();
+	    System.out.println( "Reversed String  : " +stringBuildervarible);
+
+
+	}
+
+}
