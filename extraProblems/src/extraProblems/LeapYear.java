112f763361eae623d087e85bf4c2151c0e5d7870
SifatNiloy <SifatNiloy18@gmail.com>
2024-03-08 10:13:39 +0600

    leap year

---
+++ extraProblems/src/extraProblems/LeapYear.java
@@ -0,0 +1,23 @@
+package extraProblems;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		System.out.println("Enter a year");
+		int year= input.nextInt();
+		if(year%4==0 && year%100!=0) {
+			System.out.println("Leap year");
+		}
+		else if(year%400==0 && year%100==0) {
+			System.out.println("Leap year");
+		}
+		else {
+			System.out.println("Not Leap Year");
+		}
+
+	}
+
+}
