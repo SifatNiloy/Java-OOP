61a696ebfd254b2daaa0dbdc561462392f825fd2
SifatNiloy <sifatniloy18@gmail.com>
2024-04-15 20:47:06 +0600

    parametrised Method in a class

--- OOP/src/basic/Teacher.java
+++ OOP/src/basic/Teacher.java
@@ -5,6 +5,11 @@ public class Teacher {
 		String name, gender;
 		int phone;
 		
+		void setInformation(String n, String m, int ph) {
+			name = n;
+			gender= m;
+			phone= ph;
+		}
 		void displayInformation() {
 			System.out.println(name+"\n"+gender+"\n"+phone);
 		}
--- OOP/src/basic/Test.java
+++ OOP/src/basic/Test.java
@@ -6,15 +6,11 @@ public class Test {
 		// TODO Auto-generated method stub
 		Teacher teacher1;  //object declared
 		teacher1= new Teacher();  //object created
-		teacher1.name= "Anisul Islam";
-		teacher1.gender="Male";
-		teacher1.phone=2987435;
+		teacher1.setInformation("Anisul Islam", "Male", 24358742);
 		teacher1.displayInformation();
 		
 		Teacher teacher2= new Teacher(); //object created
-		teacher2.name= "sifat";
-		teacher2.gender="Male";
-		teacher2.phone=1684345;
+		teacher2.setInformation("Sifat","Male", 49590);
 		teacher2.displayInformation();
 	}
 
