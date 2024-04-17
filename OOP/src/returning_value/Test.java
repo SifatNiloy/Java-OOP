8c9357f3706f947102ccb2bc9b3843d76a31cccd
SifatNiloy <sifatniloy18@gmail.com>
2024-04-17 11:38:16 +0600

    Returning a value from method

---
+++ OOP/src/returning_value/Test.java
@@ -0,0 +1,11 @@
+package returning_value;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ReturningValueDemo ob1= new ReturningValueDemo();
+		int result= ob1.square(5);
+		System.out.println(result);
+	}
+
+}
