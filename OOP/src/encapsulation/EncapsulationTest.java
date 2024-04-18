cb7cbf5c67e90a9a2b28994d9babd46c9f68af24
SifatNiloy <sifatniloy18@gmail.com>
2024-04-19 00:05:43 +0600

    encapsulation

---
+++ OOP/src/encapsulation/EncapsulationTest.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class EncapsulationTest {
+
+	public static void main(String[] args) {
+		Person p1= new Person();
+		p1.setName("Sifat") ;
+		p1.setAge(26) ;
+		p1.getName();
+		p1.getAge();
+	}
+
+}
---
+++ OOP/src/encapsulation/Person.java
@@ -0,0 +1,19 @@
+package encapsulation;
+
+public class Person {
+	private String name;
+	private int age;
+	
+	public void setName(String name) {
+		this.name= name;
+	}
+	public void getName() {
+		System.out.println(name);
+	}
+	public void setAge(int age) {
+		this.age= age;
+	}
+	public void getAge() {
+		System.out.println(age);
+	}
+}
